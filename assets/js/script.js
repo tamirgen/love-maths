9620449748f52af51d0d7e8a0f3fc2835027d72b
tamirgen <tamirgenkin@gmail.com>
2021-09-27 12:04:36 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
-console.log(good);
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
 
